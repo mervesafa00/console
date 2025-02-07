25494bc9c0a855f4139c83efd4af6683ef253926
Merve Safa özcan <mervesafa003@gmail.com>
2025-02-07 15:41:00 +0300

    Add project files.

---
+++ console/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace console
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           Console.Write("Bir sayı giriniz: ");
+           int sayi1 = Convert.ToInt32(Console.ReadLine());
+           Console.Write("Bir sayı daha giriniz: ");
+           int sayi2 = Convert.ToInt32(Console.ReadLine());
+           int toplam = sayi1 + sayi2;
+           Console.WriteLine("Sayıların toplamı: " + toplam);
+        }
+    }
+}
